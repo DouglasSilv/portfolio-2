1ee7d286cc2276da864d25b6af5f790c54771e9c
Douglas da Silva de Moura <douglas.silva@dbccompany.com.br>
2023-12-18 11:08:39 -0300

    add google analitycs

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "@fontsource/libre-franklin";
 import "@fontsource/inconsolata";
 import "@fontsource/inconsolata/600.css";
 import "@fontsource/inconsolata/400.css";
 import Home from '../pages/Home'
 import '../styles/styles.css'
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-5NPM6DB057');
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.send("pageview");
+  }, []);
+
   return (
     <Home/>
   )
